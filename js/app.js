b55204c9f4d48ba5be9a33bf48ddc4967647f206
Bruno Gonçalves <bruno.goncalves1999@hotmail.com>
2026-05-27 19:32:45 -0300

    correct

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ if (whatsappButton && nameInput) {
       return;
     }
 
-    const message = encodeURIComponent(`Olá, meu nome ${name} confirmo presença no seu aniversário.`);
+    const message = encodeURIComponent(`Olá, meu nome ${name} confirmo presença no aniversário da Dhiulia.`);
     const phone = '5534988767441';
     const whatsappUrl = `https://api.whatsapp.com/send?phone=${phone}&text=${message}`;
 
